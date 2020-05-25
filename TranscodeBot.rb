331bb767f725bd388216990a1f309802a519794f
Tucker Osman <osmiumusa@gmail.com>
2020-05-25 16:24:03 -0500

    add enqueue on start functionality

--- TranscodeBot.rb
+++ TranscodeBot.rb
@@ -116,6 +116,8 @@ worker = Thread.new do
   end
 end
 
+Dir['**/*'].reject {|fn| File.directory?(fn) }.each { |fn| enqueue_file(fn) } if ENV["ENQUEUE_ON_START"]
+
 notifier = INotify::Notifier.new
 notifier.watch($input_dir, :close_write, :recursive) do |event|
   if File.file?(event.absolute_name)
